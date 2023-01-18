7fc08517115292f4895d243cbadf47ca7d3eaaa3
pravesh kumar <praveshkumar.my255@gmail.com>
2023-01-18 22:12:35 +0530

    this

---
+++ stack.java
@@ -0,0 +1,17 @@
+// public void pop(){
+//     temp=-1;
+//     if (top==-1){
+//         System.out.println("Underflow");
+//     }
+//     else{
+//         int temp=arr[top];
+//         top--;
+//     }
+//     return temp;
+// }
+public class stack {
+    public static void main(String[] args) {
+        System.out.println("My name is Pravesh Kumar");
+    }
+
+}
